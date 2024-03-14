473ba0af34f20690b34d002a3e26b0cc22eaf13a
AEPALO00 <palomaresv.ae@gmail.com>
2024-03-13 23:00:20 -0400

    functions - practice

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -10,3 +10,13 @@ function calculateArea() {
     document.getElementById("result").innerText = `The area of the rectangle is: ${area}`;
 }
 
+function calculateGroceries() {
+    let q_groceries = document.querySelectorAll(".grocery");
+    let sum = 0;
+
+    q_groceries.forEach((grocery) => {
+        sum += parseFloat(grocery.value);    
+    })
+
+    document.getElementById("result_groceries").innerText = `The amount of items is: ${sum}`;
+}
