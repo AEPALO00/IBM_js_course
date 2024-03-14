1567e5c098465213e2f1555d00a158d9f7ee3f09
AEPALO00 <palomaresv.ae@gmail.com>
2024-03-13 21:28:34 -0400

    scope & variables - practice

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -19,6 +19,7 @@ console.log(globalConst); // Output: "I'm a global constant"
 // //Block Scope
 // console.log(blockVar);
 // console.log(blockLet);
+// console.log(blockConst);
 
 function show(){
 var functionVar = "I'm a block-scoped var";
@@ -27,7 +28,36 @@ const functionConst = "I'm a block-scoped const";
 }
 show();
 
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
 
+{
+    // Initial variables
+    let fullName   = "Ernesto Villarreal";
+    const age      = 25;
+    var course     = "js";
+
+    // Reassign variables
+    fullName   = "Carlos Villarreal";
+    // age     = 26; // Can't reassign; TypeError 
+    course     = "html";
+
+    console.log(fullName);   // Carlos Villarreal
+    console.log(age);        // 25
+    console.log(course);     // html
+}
+
+// console.log(fullName);   // Can't be printed
+// console.log(age);        // Can't be printed
+console.log(course);     // Can be printed
+
+console.log("-------Outside block-------")
+// Reassign variables
+fullName   = "Edgar Villarreal"; 
+age        = 26; // Can be reassigned, but is a new variable
+course     = "css";
+
+console.log(fullName);   // Edgar Villarreal
+console.log(age);        // 26
+console.log(course);     // css
