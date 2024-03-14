f26a9654481f9439204e96e737284164657423fe
AEPALO00 <palomaresv.ae@gmail.com>
2024-03-13 21:56:52 -0400

    ternary

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -48,4 +48,10 @@ switch (userType) {
 
 console.log("User Category:", userCategory);
 
+// isAuthenticated and authenticationStatus
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
 
