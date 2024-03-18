78bdf77bd7c2c6a2fda9fb07976b0f172471ab90
AEPALO00 <palomaresv.ae@gmail.com>
2024-03-17 20:31:51 -0400

    AJAX intro

---
+++ healthArticle/health_article.js
@@ -0,0 +1,16 @@
+var xhr = new XMLHttpRequest();
+
+var url = './health.json';
+
+xhr.open('GET', url, true);
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+    var articles = xhr.response.articles;
+    var articlesDiv = document.getElementById('articles');
+
+}
+
+articles.array.forEach(article => {
+    
+});
