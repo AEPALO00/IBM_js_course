7fc110c1bf5c13577a7720e4515c17c04e915ff9
AEPALO00 <palomaresv.ae@gmail.com>
2024-03-17 22:02:43 -0400

    fetch practice task

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,10 +1,14 @@
 function showweatherDetails(event) {
     event.preventDefault();
 
-    const city = document.getElementById('city').value;
+    // const city = document.getElementById('city').value;
+    const lat = document.getElementById("latitude").value;
+    const lon = document.getElementById("longitude").value;
     const apiKey = 'b938b393feee8708b324fae519b6df79'; // Replace 'YOUR_API_KEY' with your actual API key
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
+    console.log(apiUrl)
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
@@ -12,7 +16,8 @@ function showweatherDetails(event) {
         const weatherInfo = document.getElementById('weatherInfo');
         weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
                                 <p>Temperature: ${data.main.temp} &#8451;</p>
-                                <p>Weather: ${data.weather[0].description}</p>`;
+                                <p>Weather: ${data.weather[0].description}</p>
+                                <p>Feels like: ${data.main.feels_like} &#8451;`;
     })
     .catch(error => {
         console.error('Error fetching weather:', error);
